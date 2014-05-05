b35d3fba06a87c9559b242706df5ece0a7e61487
Masataka Yakura <masataka.yakura@gmail.com>
2014-05-05 15:23:43 +0900

    rewrite the extension using Promises.

--- version.js
+++ version.js
@@ -1,221 +1,183 @@
 (function () {
 
-    /**
-     * Helper function for XHR
-     * @param {string} url a URL to GET
-     * @param {function(string, Object=)} callback invoked when a request succeeds
-     * @param {Object=} annexure data which can be passed to the {@code callback}
-     */
-    var request = function (url, callback, annexure) {
-
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.onload = function () {
-            if (xhr.status === 200) {
-                callback(xhr.response, annexure);
-            } else {
-                console.error('Request failed.');
-            }
+  /**
+   * Helper function of XHR
+   * @param {string} url URL to which make an HTTP request
+   */
+  var httpget = function (url) {
+    return new Promise(function (resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', url);
+      xhr.onload = function () {
+        if (xhr.status === 200) {
+          resolve(xhr.response);
         }
-        xhr.send();
-    };
-
-    /**
-     * Obtains WebKit revision number from changeset URL.
-     * @param {string} url URL of a WebKit changeset.
-     * @return {number} revision a WebKit revision number
-     */
-    var getWebKitRevision = function (url) {
-
-        var regChangeset = /^https?:\/\/trac\.webkit\.org\/changeset\/(\d+)$/;
-
-        if (regChangeset.test(url)) {
-            return parseInt(regChangeset.exec(url).slice(1), 10);
-        } else {
-            console.error('Invalid argument: ', url);
-            return;
+        else {
+          reject(new Error(xhr.status));
         }
-    };
-
-    /**
-     * Obtains WebKit version number from revision number.
-     * @param {(number|string)} revision revision number of a WebKit changeset.
-     * @return {Array.<string>} an array of strings made by MAJOR_VERSION and MINOR_VERSION
-     */
-    var getWebKitVersion = function (revision) {
-
-        var rev = parseInt(revision, 10);
-
-        if (rev < 20261) {
-            console.error('There was no conf file before r20261.');
-            return;
-        }
-
-        var url = 'http://trac.webkit.org/export/' + rev +
-                  (rev >= 75314 ? '/trunk/Source' : '/trunk') +
-                  '/WebCore/Configurations/Version.xcconfig';
-
-        var extractWKVersion = function (response) {
-            var data = response,
-                regVersions = /MAJOR_VERSION = (\d{3});\nMINOR_VERSION = (\d{1,2});/;
-
-            if (regVersions.test(data)) {
-                var version = regVersions.exec(data).slice(1).join('.');
-                var ev = new CustomEvent('WKVersionObtained', { "detail": { "version": version }});
-                window.dispatchEvent(ev);
-            } else {
-                console.error('Cannot obtain version from the data.');
-            }
-        };
-
-        request(url, extractWKVersion);
-    };
-
-    /**
-     * Canonicalizes the version string by zero-padding for making it easier in comparison.
-     * @param {(Array.<string>|string)} version WebKit version string
-     * @return {string} canonicalized version string (zero-padded)
-     */
-    var canonicalizeWebKitVersion = function (version) {
-
-        if (Array.isArray(version) && version.length >= 2) {
-            version = version.join('.');
-        }
-
-        var regVersion = /^(\d{3})\.(\d{1,2})\.?\d*$/;
-        if (typeof version === 'string' && regVersion.test(version)) {
-            return version.replace(regVersion, function(m, p1, p2) {
-                return p1 + '.' + ('0' + p2).slice(-2);
-            });
-        } else {
-            console.error('Invalid argument: ', version);
-            return;
-        }
-    };
-
-    /**
-     * Returns the earliest version of a browser that (might) support the feature on the changeset
-     * @param {Array.<Object>} browser an array contains objects of product and webkit version numbers
-     * @param {string} version WebKit version string
-     * @return {string} a browser version number
-     */
-    var findBrowserVersion = function (browser, version) {
-
-        if (!Array.isArray(browser) || browser.length === 0) {
-            console.error('Invalid argument: ', browser);
-            return;
-        }
-
-        if (!('product' in browser[0]) || !('webkit' in browser[0])) {
-            console.error('Invalid argument: ', browser[0]);
-            return;
-        }
-
-        version = canonicalizeWebKitVersion(version);
-
-        var i = 0,
-            l = browser.length,
-            candidate;
-
-        if (version > browser[0].webkit) { return 'Nightly'; }
-        if (version < browser[l-1].webkit) { return ''; }
-
-        while (i < l) {
-            if (version <= browser[i].webkit) {
-                candidate = i;
-            } else {
-                return browser[candidate].product;
-            }
-            i++;
-        }
-    };
-
-    /**
-     * Returns the earliest Safari version that (might) support the feature on the changeset
-     * @param {string} version canonicalized version string
-     * @return {string} Safari version number
-     */
-    var findSafariVersion = function (version) {
-
-        var safari = [
-            { "product": "7.0", "webkit": "537.71" },
-            { "product": "6.0", "webkit": "536.25" },
-            { "product": "5.1", "webkit": "534.48.3" },
-            { "product": "5.0", "webkit": "534.22.3" },
-            { "product": "4.1", "webkit": "533.19.4" },
-            { "product": "4.0", "webkit": "531.22.7" },
-            { "product": "3.2", "webkit": "525.28" },
-            { "product": "3.1", "webkit": "525.21" },
-            { "product": "3.0", "webkit": "523.10" }
-        ];
-
-        return findBrowserVersion(safari, version);
-    };
-
-    /**
-     * Returns the earliest Chrome version that (might) support the feature on the changeset
-     * @param {string} version canonicalized version string
-     * @return {string} Chrome version number
-     */
-    var findChromeVersion = function (version) {
-
-        var chrome = [
-            { "product": "27", "branch": "1453", "webkit": "537.36" },
-            { "product": "26", "branch": "1410", "webkit": "537.31" },
-            { "product": "25", "branch": "1364", "webkit": "537.22" },
-            { "product": "24", "branch": "1312", "webkit": "537.17" },
-            { "product": "23", "branch": "1271", "webkit": "537.11" },
-            { "product": "22", "branch": "1229", "webkit": "537.04" },
-            { "product": "21", "branch": "1180", "webkit": "537.01" },
-            { "product": "20", "branch": "1132", "webkit": "536.11" },
-            { "product": "19", "branch": "1084", "webkit": "536.05" },
-            { "product": "18", "branch": "1025", "webkit": "535.19" },
-            { "product": "17", "branch": "963",  "webkit": "535.11" },
-            { "product": "16", "branch": "912",  "webkit": "535.07" },
-            { "product": "15", "branch": "874",  "webkit": "535.02" },
-            { "product": "13/14", "branch": "835",  "webkit": "535.01" },
-            { "product": "12", "branch": "742",  "webkit": "534.30" },
-            { "product": "11", "branch": "696",  "webkit": "534.24" },
-            { "product": "10", "branch": "648",  "webkit": "534.16" },
-            { "product": "9",  "branch": "597",  "webkit": "534.13" },
-            { "product": "8",  "branch": "552",  "webkit": "534.10" },
-            { "product": "7",  "branch": "517",  "webkit": "534.07" },
-            { "product": "6",  "branch": "472",  "webkit": "534.03" },
-            { "product": "5",  "branch": "375",  "webkit": "533.04" },
-            { "product": "4",  "branch": "249",  "webkit": "532.05" },
-            { "product": "3",  "branch": "195",  "webkit": "532.00" },
-            { "product": "2",  "branch": "172",  "webkit": "530.05" },
-            { "product": "1",  "branch": "154",  "webkit": "525.19" },
-        ];
-            /* TODO: find a way to distinguish Cr13 and Cr14
-            { "product": "14", "branch": "835",  "webkit": "535.1" },
-            { "product": "13", "branch": "782",  "webkit": "535.1" },
-            */
-
-        return findBrowserVersion(chrome, version);
-    };
-
-    /**
-     * Adds corresponding WebKit version to the changeset heading.
-     * @param {string} version WebKit version string
-     */
-    var updateChangesetHeading = function (version) {
-        var safariVersion = findSafariVersion(version);
-        var chromeVersion = findChromeVersion(version);
-        document.querySelector('h1').textContent +=
-            ' (' + version + ' | Safari ' + safariVersion + ((chromeVersion !== 'Nightly') ? ' | Chrome ' + chromeVersion : '') + ')';
-    };
-
-
-    /* init */
-    var url = location.href,
-        rev = getWebKitRevision(url);
-
-    window.addEventListener('WKVersionObtained', function (e) {
-        var version = e.detail.version;
-        updateChangesetHeading(version);
+      }
+      xhr.onerror = function () {
+        reject(new Error(xhr.status));
+      }
+      xhr.send();
     });
+  }
 
-    getWebKitVersion(rev);
+  /**
+   * Get WebKit version string from an HTTP response
+   * @param {string} url URL of the changeset
+   */
+  var getConfigURL = function (url) {
+    return new Promise(function (resolve, reject) {
+      var reChangeset = /^https?:\/\/trac\.webkit\.org\/changeset\/(\d+)$/;
+        if (reChangeset.test(url)) {
+          var revision = parseInt(reChangeset.exec(url).slice(1));
+          if (revision >= 20261) {
+            resolve('http://trac.webkit.org/export/' + revision +
+                    (revision >= 75314 ? '/trunk/Source' : '/trunk') +
+                    '/WebCore/Configurations/Version.xcconfig');
+          }
+          else {
+            reject(new Error('No Version.xcconfig before r20261.'));
+          }
+        }
+        else {
+          reject(new Error('Invalid URL.'));
+        }
+    });
+  }
+
+  /**
+   * Get WebKit version string from an HTTP response
+   * @param {string} response HTTP response from Version.xcconfig
+   */
+  var getWebKitVersion = function (response) {
+    return new Promise(function (resolve, reject) {
+      var reVersions = /MAJOR_VERSION = (\d{3});\nMINOR_VERSION = (\d{1,2});/;
+      if (reVersions.test(response)) {
+        var version = reVersions.exec(response).slice(1).join('.')
+          .replace(/(\d{3})\.(\d{1,2})/, function(m, p1, p2) {
+            return p1 + '.' + ('0' + p2).slice(-2);
+          });
+        resolve(version);
+      } else {
+        reject(new Error('Cannot obtain version from the response.'));
+      }
+    });
+  };
+
+  /**
+   * Update the heading of the changest
+   * @param {string} version WebKit version string
+   */
+  var updatePage = function (version) {
+    var safariVersion = findSafariVersion(version);
+    var chromeVersion = findChromeVersion(version);
+    document.querySelector('h1').textContent += ' (' + version + ' | Safari ' + safariVersion + ((chromeVersion !== 'Nightly') ? ' | Chrome ' + chromeVersion : '') + ')';
+  }
+
+  /**
+   * Returns the earliest version of a browser that (might) support the feature on the changeset
+   * @param {Array.<Object>} browser an array contains objects of product and webkit version numbers
+   * @param {string} version WebKit version string
+   * @return {string} a browser version number
+   */
+  var findBrowserVersion = function (browser, version) {
+    if (!Array.isArray(browser) || browser.length === 0) {
+      console.error('Invalid argument: ', browser);
+      return;
+    }
+    if (!('product' in browser[0]) || !('webkit' in browser[0])) {
+      console.error('Invalid argument: ', browser[0]);
+      return;
+    }
+    var i = 0,
+        l = browser.length,
+        candidate;
+    if (version > browser[0].webkit) { return 'Nightly'; }
+    if (version < browser[l-1].webkit) { return ''; }
+    while (i < l) {
+      if (version <= browser[i].webkit) {
+        candidate = i;
+      } else {
+        return browser[candidate].product;
+      }
+      i++;
+    }
+  };
+
+  /**
+   * Returns the earliest Safari version that (might) support the feature on the changeset
+   * @param {string} version canonicalized version string
+   * @return {string} Safari version number
+   */
+  var findSafariVersion = function (version) {
+    var safari = [
+      { "product": "7.0", "webkit": "537.71" },
+      { "product": "6.0", "webkit": "536.25" },
+      { "product": "5.1", "webkit": "534.48.3" },
+      { "product": "5.0", "webkit": "534.22.3" },
+      { "product": "4.1", "webkit": "533.19.4" },
+      { "product": "4.0", "webkit": "531.22.7" },
+      { "product": "3.2", "webkit": "525.28" },
+      { "product": "3.1", "webkit": "525.21" },
+      { "product": "3.0", "webkit": "523.10" }
+    ];
+    return findBrowserVersion(safari, version);
+  };
+
+  /**
+   * Returns the earliest Chrome version that (might) support the feature on the changeset
+   * @param {string} version canonicalized version string
+   * @return {string} Chrome version number
+   */
+  var findChromeVersion = function (version) {
+    var chrome = [
+      { "product": "27", "branch": "1453", "webkit": "537.36" },
+      { "product": "26", "branch": "1410", "webkit": "537.31" },
+      { "product": "25", "branch": "1364", "webkit": "537.22" },
+      { "product": "24", "branch": "1312", "webkit": "537.17" },
+      { "product": "23", "branch": "1271", "webkit": "537.11" },
+      { "product": "22", "branch": "1229", "webkit": "537.04" },
+      { "product": "21", "branch": "1180", "webkit": "537.01" },
+      { "product": "20", "branch": "1132", "webkit": "536.11" },
+      { "product": "19", "branch": "1084", "webkit": "536.05" },
+      { "product": "18", "branch": "1025", "webkit": "535.19" },
+      { "product": "17", "branch": "963",  "webkit": "535.11" },
+      { "product": "16", "branch": "912",  "webkit": "535.07" },
+      { "product": "15", "branch": "874",  "webkit": "535.02" },
+      { "product": "13/14", "branch": "835",  "webkit": "535.01" },
+      { "product": "12", "branch": "742",  "webkit": "534.30" },
+      { "product": "11", "branch": "696",  "webkit": "534.24" },
+      { "product": "10", "branch": "648",  "webkit": "534.16" },
+      { "product": "9",  "branch": "597",  "webkit": "534.13" },
+      { "product": "8",  "branch": "552",  "webkit": "534.10" },
+      { "product": "7",  "branch": "517",  "webkit": "534.07" },
+      { "product": "6",  "branch": "472",  "webkit": "534.03" },
+      { "product": "5",  "branch": "375",  "webkit": "533.04" },
+      { "product": "4",  "branch": "249",  "webkit": "532.05" },
+      { "product": "3",  "branch": "195",  "webkit": "532.00" },
+      { "product": "2",  "branch": "172",  "webkit": "530.05" },
+      { "product": "1",  "branch": "154",  "webkit": "525.19" },
+    ];
+      /* ISSUE: no way to distinguish Cr13 and Cr14
+      { "product": "14", "branch": "835",  "webkit": "535.1" },
+      { "product": "13", "branch": "782",  "webkit": "535.1" },
+      */
+    return findBrowserVersion(chrome, version);
+  };
+
+  // kick off
+  (function () {
+    getConfigURL(location.href)
+      .then(httpget)
+      .then(getWebKitVersion)
+      .then(function (version) {
+        updatePage(version);
+      })
+      .catch(function(error) {
+        console.error(error);
+      })
+  }());
 
 }());
