7eb16d0ac1e7a14159c4ee018fb1fe0b30f5ad91
Masataka Yakura <masataka.yakura@gmail.com>
2015-07-31 23:45:44 +0900

    update dicentra_title.js to use ES6

--- dicentra_title.js
+++ dicentra_title.js
@@ -1,18 +1,17 @@
-(function () {
-  var message = document.querySelector('dd.message p').textContent
-  var re = /https?:\/\/bugs\.webkit\.org\/show_bug\.cgi\?id=\d+|<\s*(?:https?:\/\/webkit\.org\/b|rdar:\/\/problem)\/\d+>/g
-  /*
-  bugzilla:  /https?:\/\/bugs\.webkit\.org\/show_bug\.cgi\?id=\d+/g
-  shortened: /<\s*https?:\/webkit\.org\/b\/\d+>/g
-  radar url: /<\s*rdar:\/\/problem\/\d+>/g
-  */
+'use strict'
 
-  message = message.replace(re, '').trim()
+let messageContent = document.querySelector('dd.message p').textContent
+const reBugURLs = /https?:\/\/bugs\.webkit\.org\/show_bug\.cgi\?id=\d+|<\s*(?:https?:\/\/webkit\.org\/b|rdar:\/\/problem)\/\d+>/g
+/*
+bugzilla:  /https?:\/\/bugs\.webkit\.org\/show_bug\.cgi\?id=\d+/g
+shortened: /<\s*https?:\/webkit\.org\/b\/\d+>/g
+radar url: /<\s*rdar:\/\/problem\/\d+>/g
+*/
 
-  // if there are multiple "paragraphs" in the element, grab the first one
-  if (/\n\s*\n/.test(message)) {
-    message = message.split(/\n\s*\n/)[0]
-  }
-  document.title += ' \u2014 ' + message.replace(/\n/, ' ')
-}())
+let message = messageContent.replace(reBugURLs, '').trim()
 
+// if there are multiple "paragraphs" in the element, grab the first one
+if (/\n\s*\n/.test(message)) {
+  message = message.split(/\n\s*\n/)[0]
+}
+document.title += ` ${'\u2014'} ${message.replace(/\n/, ' ')}`
